3181e5223ed088be56f8fe1f86ddd5a4f4a1fa3d
Kelvin Godliver <84456676+breldosg@users.noreply.github.com>
2023-08-30 10:06:28 -0700

    good mody

---
+++ assets/js/js.js
@@ -0,0 +1,33 @@
+const moonRoot = document.getElementById('moonRoot');
+const sunRoot = document.getElementById('sunRoot');
+const body = document.body;
+
+body.classList.remove('night-mode');
+body.classList.add('light-mode');
+
+moonRoot.addEventListener('click', activateNightMode);
+sunRoot.addEventListener('click', activateLightMode);
+
+function activateNightMode() {
+    body.classList.remove('light-mode');
+    body.classList.add('night-mode');
+    fadeOutAndSwap('#moonRoot', '#sunRoot');
+}
+
+function activateLightMode() {
+    body.classList.remove('night-mode');
+    body.classList.add('light-mode');
+    fadeOutAndSwap('#sunRoot', '#moonRoot');
+}
+
+function fadeOutAndSwap(fadeOutSelector, fadeInSelector) {
+    const fadeOutElement = document.querySelector(fadeOutSelector);
+    fadeOutElement.style.opacity = '0';
+
+    setTimeout(() => {
+        fadeOutElement.style.display = 'none';
+        const fadeInElement = document.querySelector(fadeInSelector);
+        fadeInElement.style.display = 'flex';
+        fadeInElement.style.opacity = '1';
+    }, 300); // Delay for fade out effect
+}
