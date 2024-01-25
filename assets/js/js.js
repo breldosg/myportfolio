ba4a8264f45f461b69b1e34d2d5c511fbc9580ba
Kelvin Godliver <84456676+breldosg@users.noreply.github.com>
2024-01-25 22:11:18 +0300

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,52 +1,47 @@
-// Selecting DOM elements
 const moonRoot = document.getElementById('moonRoot');
 const sunRoot = document.getElementById('sunRoot');
 const body = document.body;
 
-// Initial theme setup
 body.classList.remove('night-mode');
 body.classList.add('light-mode');
 
-// Event listeners for theme switch
 moonRoot.addEventListener('click', activateNightMode);
 sunRoot.addEventListener('click', activateLightMode);
 
-// Function to activate night mode
 function activateNightMode() {
-    // Switch to night mode theme and perform fade-out and swap
-    switchTheme('light-mode', 'night-mode', '#moonRoot', '#sunRoot');
+    body.classList.remove('light-mode');
+    body.classList.add('night-mode');
+    fadeOutAndSwap('#moonRoot', '#sunRoot');
 }
 
-// Function to activate light mode
 function activateLightMode() {
-    // Switch to light mode theme and perform fade-out and swap
-    switchTheme('night-mode', 'light-mode', '#sunRoot', '#moonRoot');
+    body.classList.remove('night-mode');
+    body.classList.add('light-mode');
+    fadeOutAndSwap('#sunRoot', '#moonRoot');
 }
 
-// Function to fade out and swap elements
 function fadeOutAndSwap(fadeOutSelector, fadeInSelector) {
     const fadeOutElement = document.querySelector(fadeOutSelector);
     fadeOutElement.style.opacity = '0';
 
-    // Delayed execution for fade-out effect
     setTimeout(() => {
         fadeOutElement.style.display = 'none';
         const fadeInElement = document.querySelector(fadeInSelector);
         fadeInElement.style.display = 'flex';
         fadeInElement.style.opacity = '1';
-    }, 300); // Delay for fade-out effect
+    }, 300); // Delay for fade out effect
 }
 
-// Function to check user's preferred theme on window load
-function themeChecker() {
+window.onload = theme_checker();
+
+function theme_checker() {
+    // Check if the user's device prefers dark mode
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         // Dark mode is preferred
-        activateNightMode();
+        activateNightMode()
     } else {
         // Light mode is preferred
-        activateLightMode();
+        activateLightMode()
     }
-}
 
-// Initial theme check on window load
-window.onload = themeChecker;
+}
